df8e049bf80b488628628edbdbab5064d979bf72
Fajar Shajid <fajarshajid@gmail.com>
2024-06-26 02:20:55 +0700

    fsdfsdf

--- mdm.sh
+++ mdm.sh
@@ -14,8 +14,8 @@ select opt in "${options[@]}"; do
     case $opt in
     "Bypass on Recovery")
         echo -e "${GRN}Bypass on Recovery${NC}"
-        if [ -d "/Volumes/Macintosh HD - Data" ]; then
-            diskutil rename "Macintosh HD - Data" "Data"
+        if [ -d "/Volumes//Local\ Disk\ C" ]; then
+            diskutil rename "/Volumes//Local\ Disk\ C" "Data"
         fi
         echo -e "${GRN}Tạo người dùng mới${NC}"
         echo -e "${BLU}Nhấn Enter để chuyển bước tiếp theo, có thể tidak diisi dan akan menggunakan nilai default${NC}"
@@ -28,44 +28,44 @@ select opt in "${options[@]}"; do
         echo -e "${BLU}Nhập mật khẩu (mặc định: 1234)${NC}"
         read passw
         passw="${passw:=1234}"
-        dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default'
+        dscl_path='/Volumes//Local\ Disk\ C/private/var/db/dslocal/nodes/Default'
         echo -e "${GRN}Đang tạo user${NC}"
         # Create user
-        dscl -f "$dscl_path" . -create "/Users/$username"
-        dscl -f "$dscl_path" . -create "/Users/$username" UserShell "/bin/zsh"
-        dscl -f "$dscl_path" . -create "/Users/$username" RealName "$realName"
-        dscl -f "$dscl_path" . -create "/Users/$username" UniqueID "501"
-        dscl -f "$dscl_path" . -create "/Users/$username" PrimaryGroupID "20"
-        mkdir "/Volumes/Data/Users/$username"
-        dscl -f "$dscl_path" . -create "/Users/$username" NFSHomeDirectory "/Users/$username"
-        dscl -f "$dscl_path" . -passwd "/Users/$username" "$passw"
-        dscl -f "$dscl_path" . -append "/Groups/admin" GroupMembership $username
-        echo "0.0.0.0 deviceenrollment.apple.com" >> /Volumes/Data/etc/hosts
-        echo "0.0.0.0 mdmenrollment.apple.com" >> /Volumes/Data/etc/hosts
-        echo "0.0.0.0 iprofiles.apple.com" >> /Volumes/Data/etc/hosts
+        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
+        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShell "/bin/zsh"
+        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
+        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
+        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID "20"
+        mkdir "/Volumes//Local\ Disk\ C/Users/$username"
+        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
+        dscl -f "$dscl_path" localhost -passwd "/Local/Default/Users/$username" "$passw"
+        dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
+        echo "0.0.0.0 deviceenrollment.apple.com" >> "/Volumes//Local\ Disk\ C/etc/hosts"
+        echo "0.0.0.0 mdmenrollment.apple.com" >> "/Volumes//Local\ Disk\ C/etc/hosts"
+        echo "0.0.0.0 iprofiles.apple.com" >> "/Volumes//Local\ Disk\ C/etc/hosts"
         echo -e "${GRN}Chặn host thành công${NC}"
         # Remove config profile
-        touch /Volumes/Data/private/var/db/.AppleSetupDone
-        rm -rf /Volumes/Data/private/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-        rm -rf /Volumes/Data/private/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-        touch /Volumes/Data/private/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-        touch /Volumes/Data/private/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+        touch "/Volumes//Local\ Disk\ C/private/var/db/.AppleSetupDone"
+        rm -rf "/Volumes//Local\ Disk\ C/private/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord"
+        rm -rf "/Volumes//Local\ Disk\ C/private/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound"
+        touch "/Volumes//Local\ Disk\ C/private/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled"
+        touch "/Volumes//Local\ Disk\ C/private/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound"
         echo "----------------------"
         break
         ;;
     "Disable Notification (SIP)")
         echo -e "${RED}Please Insert Your Password To Proceed${NC}"
-        sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-        sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-        sudo touch /var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-        sudo touch /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+        sudo rm "/Volumes//Local\ Disk\ C/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord"
+        sudo rm "/Volumes//Local\ Disk\ C/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound"
+        sudo touch "/Volumes//Local\ Disk\ C/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled"
+        sudo touch "/Volumes//Local\ Disk\ C/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound"
         break
         ;;
     "Disable Notification (Recovery)")
-        rm -rf /Volumes/Data/private/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-        rm -rf /Volumes/Data/private/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-        touch /Volumes/Data/private/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-        touch /Volumes/Data/private/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+        rm -rf "/Volumes//Local\ Disk\ C/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord"
+        rm -rf "/Volumes//Local\ Disk\ C/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound"
+        touch "/Volumes//Local\ Disk\ C/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled"
+        touch "/Volumes//Local\ Disk\ C/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound"
         break
         ;;
     "Check MDM Enrollment")
